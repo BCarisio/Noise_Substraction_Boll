60a7db7ac044d5895b02ff84c6bf3b533f27e3a1
BCarisio <carisiobenjamin2@gmail.com>
2022-11-12 15:00:59 -0300

    Commit inicial (Vacío)

---
+++ func.py
@@ -0,0 +1,7 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Nov 12 14:54:21 2022
+
+@author: Benjamín Carisio
+"""
+
---
+++ main.py
@@ -0,0 +1,7 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Nov 12 14:53:56 2022
+
+@author: Benjamín Carisio
+"""
+
