ce1b1c24bcb04a8e86b601179bd07f21797c389e
unknown <cohen51134@estudiantes.untref.edu.ar>
2022-11-21 00:07:30 -0300

    Código final. func.py y main.py terminados.

--- func.py
+++ func.py
@@ -1,6 +1,4 @@
 import numpy as np
-import matplotlib.pyplot as plt
-import soundfile as sf
 from scipy.fft import fft, ifft
 
 def Boll_base(señal, fs, long=50, umbral=8):
@@ -67,39 +65,9 @@ def Boll_base(señal, fs, long=50, umbral=8):
         array_hamming[i] = array_ventaneo[i]*ventana_hamming                        # Por otro lado, se aplica la ventana de Hamming a las ventanas.
         salida_ste[i] = np.sum(abs(array_hamming[i])**2)/cant_muestras_ventana      # STE de la señal
     
-    
-    
-    # #Se aplica la ventana de Hanning a cada ventana
-    # array_hanning = np.zeros((cant_ventanas,cant_muestras_ventana))
-    # n = np.arange(0,cant_muestras_ventana)
-    # ventana_hanning = 0.5 - 0.5*np.cos((2*np.pi*n)/(cant_muestras_ventana-1))
-    # for i in range(0,cant_ventanas):
-    #     array_hanning[i] = array_ventaneo[i]*ventana_hanning
-
-    # #Se aplica FFT a cada una de las ventanas
-    # array_fft = np.zeros((cant_ventanas,cant_muestras_ventana), dtype=complex)
-    # for i in range(0,cant_ventanas):
-    #     array_fft[i] = fft(array_hanning[i])
-    
-    
-    
-    # array_hamming = np.zeros((cant_ventanas,cant_muestras_ventana))
-    # ventana_hamming = 0.54 - 0.46*np.cos((2*np.pi*n)/(cant_muestras_ventana-1))
-    # for i in range(0,cant_ventanas):
-    #     array_hamming[i] = array_ventaneo[i]*ventana_hamming
-    
-    # salida_ste = np.zeros(cant_ventanas)
-    # for i in range(0,cant_ventanas):
-    #     salida_ste[i] = np.sum(abs(array_hamming[i])**2)/cant_muestras_ventana
-    
-    # Spectral Subtraction Estimator
+    # Detección de momentos sin habla
     
     valor_ref = umbral*np.min(salida_ste)             # Valor de referencia del STE a partir del cual se considera presencia de habla
-    
-    # array_sin_habla = array_hanning
-    # for i in range(len(salida_ste)):
-    #     if salida_ste[i] > valor_ref:
-    #         array_sin_habla[i] = np.zeros(cant_muestras_ventana)
     
     array_sin_habla = np.array([])
     
@@ -110,7 +78,7 @@ def Boll_base(señal, fs, long=50, umbral=8):
     
     mu = np.mean(array_sin_habla)                      # Esperanza de la magnitud espectral del ruido
     
-    spectral_estimator = (1-mu/(abs(array_fft)))*array_fft         # Spectral Subtraction Estimator
+    # spectral_estimator = (1-mu/(abs(array_fft)))*array_fft         # Spectral Subtraction Estimator
     
     
     # Magnitude Averaging
@@ -201,15 +169,11 @@ def Boll_base(señal, fs, long=50, umbral=8):
         
         else:
             spectral_estimator_att[i] = (10**(-30/20))*spectral_estimator_rnr[i]
-
-    
-    # print(spectral_estimator_att,np.shape(spectral_estimator_att))
     
     return spectral_estimator_att
     
     
-
-def synthesis(x,fs):
+def synthesis(x):
     
     # Synthesis
     
@@ -234,20 +198,318 @@ def synthesis(x,fs):
     
         señal_filtrada[i*int(long_ventana/2):i*int(long_ventana/2)+long_ventana] += array_ifft[i]
     
-    print(señal_filtrada,np.shape(señal_filtrada))
-    
     return señal_filtrada
     
 
-#%%
-import_s = sf.read('ClarinetHiNoise.wav')
-signal = import_s[0]
-fs = import_s[1]
-signal_L = np.transpose(signal)[0]
+def Boll_alt(señal, fs, long=50, umbral=8, alfa=5, beta=0.02):
+    """
+    Se aplica el método de Boll para substracción de ruido,
+    utilizando la Substracción Espectral No Lineal.
 
-BB = Boll_base(signal_L, fs)
+    Parametros
+    ----------
+    señal : Array
+        Señal a aplicar el algoritmo.
+    fs : Int
+        Frecuncia de muestreo.
+    long : Int, opcional
+        Longitud de la ventana en ms. Predeterminado en 50 ms.
+    umbral: Int, opcional
+        Parámetro para establecer el valor del ruido a filtrar. Predeterminado en 8.
+    alfa: float, opcional
+        Factor de sobre estimación (valores mayores o iguales a 1). Predeterminado en 5.
+    beta: float, opcional
+        Parámetro de piso espectral (valores entre 0 y 1). Predeterminado en 0.02.
+    Salida:
+    Array procesado
+    """
+    
+    #Conversión de ventana de tiempo a muestras
+    
+    cant_muestras_ventana = int(np.round(long*fs/1000,0))
+    
+    if cant_muestras_ventana%2==1:               # Se hace par la cantidad de muestras de ventana para un overlap simétrico
+        cant_muestras_ventana += 1
+    
+    cant_ventanas = int(len(señal)/(cant_muestras_ventana/2)-1)
+    
+    #Se ventanea la señal y se agrega a una matriz
+    
+    array_ventaneo = np.zeros((cant_ventanas,cant_muestras_ventana))        # Matriz de ventanas de la señal (f: cant. ventanas, c: cant. muestras)
+    
+    overlap = int(cant_muestras_ventana/2)
+    
+    # Definición ventana de Hanning
+    
+    array_hanning = np.zeros((cant_ventanas,cant_muestras_ventana))
+    
+    n = np.arange(0,cant_muestras_ventana)
+    
+    ventana_hanning = 0.5 - 0.5*np.cos((2*np.pi*n)/(cant_muestras_ventana-1))
+    
+    
+    array_fft = np.zeros((cant_ventanas,cant_muestras_ventana), dtype=complex)
+    
+    
+    # Definición ventana de Hamming (para estimación del STE)
+    
+    array_hamming = np.zeros((cant_ventanas,cant_muestras_ventana))
+    
+    ventana_hamming = 0.54 - 0.46*np.cos((2*np.pi*n)/(cant_muestras_ventana-1))
+    
+    salida_ste = np.zeros(cant_ventanas) # Data del STE de cada ventana de la señal
+    
+    for i in range(0,cant_ventanas):        
+        
+        array_ventaneo[i] = señal[i*overlap:cant_muestras_ventana+i*overlap]        # Ventaneo de la señal con la superposición especificada
+        array_hanning[i] = array_ventaneo[i]*ventana_hanning                        # Aplicación de ventana de Hanning
+        array_fft[i] = fft(array_hanning[i])                                        # Array de las fft de cada ventana de la señal    
+        
+        # STE
+        
+        array_hamming[i] = array_ventaneo[i]*ventana_hamming                        # Por otro lado, se aplica la ventana de Hamming a las ventanas.
+        salida_ste[i] = np.sum(abs(array_hamming[i])**2)/cant_muestras_ventana      # STE de la señal
+    
+    
+    valor_ref = umbral*np.min(salida_ste)             # Valor de referencia del STE a partir del cual se considera presencia de habla
+    
+    
+    array_sin_habla = np.array([])
+    
+    for i in range(len(salida_ste)):                   # Se crea un array de ffts del ruido ventaneado
+        if salida_ste[i] < valor_ref:          
+            array_sin_habla = np.append(array_sin_habla, abs(array_fft[i]))     # Array de fft del ruido
+    
+    
+    mu = np.mean(array_sin_habla)                      # Esperanza de la magnitud espectral del ruido
+    
+    # spectral_estimator = (1-mu/(abs(array_fft)))*array_fft         # Spectral Subtraction Estimator
+    
+    
+    # Spectral Substraction with Over Subtraction
+    
+    array_ss = np.zeros(np.shape(array_fft), dtype=complex) # Array de salida. Ventanas a las que se les aplica la sobre substracción.
+    
+    # Se define el promedio de estimación de ruido espectral
+    
+    D = np.zeros(cant_muestras_ventana, dtype=complex)   # Fila de ceros para establecer dimensión
+    
+    for i in range(len(salida_ste)):            # Se crea un array sin habla de la señal procesada
+        if salida_ste[i] < valor_ref:          
+            D = np.vstack([D, array_fft[i]])
+            
+    D = D[1:]           # Se elimina la primera fila de ceros
+    
+    # theta = np.angle(array_fft)      # Fase theta para aplicar al ruido 
+    
+    D = np.mean(D, axis=0)           # Promedios de ruido por frecuencia
+    
+    D_matriz = np.zeros(np.shape(array_fft), dtype=complex)
+    
+    for i in range(0,cant_ventanas):                # Matriz de promedios de magnitud de ruido de misma dimension que array_fft
+        
+        D_matriz[i] = D
+    
+    for  i in range(0,cant_ventanas):
+        
+        for w in range(0,cant_muestras_ventana):
+            
+            if abs(array_fft[i][w])**2 > (alfa+beta)*abs(D_matriz[i][w])**2:
+                
+                array_ss[i][w] = np.sqrt(abs(array_fft[i][w])**2 - abs(D_matriz[i][w])**2)
+            
+            else:
+                
+                array_ss[i][w] = np.sqrt(beta*abs(D_matriz[i][w])**2)
+    
+    # Half-Wave Rectification
+    
+    H = np.sqrt(1-(D_matriz/array_fft)**2) # Defino una función transferencia a partir de la salida y entrada de la sobre substracción
+    
+    Hr = (H + abs(H))/2                     # Defino el Hr con la mejora del 
+    
+    spectral_estimator_hwr = Hr*array_ss                       # S.E. post MA y HWR
 
-#%%
-synthesis(BB,fs)
+    # Residual Noise Reduction
+    
+    array_sin_habla_rnr = np.zeros(cant_muestras_ventana, dtype=complex)   # Fila de ceros para establecer dimensión
+    
+    for i in range(len(salida_ste)):            # Se crea un array sin habla de la señal procesada
+        if salida_ste[i] < valor_ref:          
+            array_sin_habla_rnr = np.vstack([array_sin_habla_rnr, spectral_estimator_hwr[i]])
+    
+    array_sin_habla_rnr = array_sin_habla_rnr[1:]           # Se elimina la primera fila de ceros
+    
+    phi = np.angle(array_sin_habla_rnr)      # Fase phi del ruido procesado
+    
+    Nr = array_sin_habla_rnr - mu*np.exp(1j*phi)                       # Noise Residual
+    
+    Nrmax = np.max(abs(Nr),axis=0)                    # Array de valores máximos del Nr por frecuencia
+    
+    spectral_estimator_rnr = np.zeros((cant_ventanas,cant_muestras_ventana),dtype=complex)   
+    
+    for i in range(cant_ventanas):              # Esquema de Residual Noise Reduction. Cuando corresponde, se asigna magnitud (nueva) y fase (del S post HWR)
+        
+        for w in range(cant_muestras_ventana):
+            if i==0:                            # 1er ventana. Se compara entre la actual y la siguiente.
+                
+                if abs(spectral_estimator_hwr[i][w])>Nrmax[w]:
+                    
+                    spectral_estimator_rnr[i][w] = spectral_estimator_hwr[i][w]
+                    
+                else:
+                    
+                    spectral_estimator_rnr[i][w] = np.min([abs(spectral_estimator_hwr[i][w]),abs(spectral_estimator_hwr[i+1][w])])*np.exp(1j*np.angle(spectral_estimator_hwr[i][w]))
+             
+            elif i==cant_ventanas-1:              # Última ventana. Se compara entre la actual y la anterior.
+                
+                if abs(spectral_estimator_hwr[i][w])>Nrmax[w]:
+                    
+                    spectral_estimator_rnr[i][w] = spectral_estimator_hwr[i][w]
+                    
+                else:
+                    
+                    spectral_estimator_rnr[i][w] = np.min([abs(spectral_estimator_hwr[i-1][w]),abs(spectral_estimator_hwr[i][w])])*np.exp(1j*np.angle(spectral_estimator_hwr[i][w]))
+                
+            else:                               # Comparación entre valor de ventana actual, anterior y posterior.
+                
+                if abs(spectral_estimator_hwr[i][w])>Nrmax[w]:
+                    
+                    spectral_estimator_rnr[i][w] = spectral_estimator_hwr[i][w]
+                    
+                else:
+                    
+                    spectral_estimator_rnr[i][w] = np.min([abs(spectral_estimator_hwr[i-1][w]),abs(spectral_estimator_hwr[i][w]),abs(spectral_estimator_hwr[i+1][w])])*np.exp(1j*np.angle(spectral_estimator_hwr[i][w]))
 
-sf.write('Salida2.wav',np.real(synthesis(BB,fs))/np.max(np.real(synthesis(BB,fs))),fs)
+
+    # Additional Signal Attenuation During Nonspeech Activity
+    
+    T = np.zeros(cant_ventanas,dtype=complex)
+    
+    for i in range(cant_ventanas):
+         
+        T[i] = 20*np.log10(np.mean(np.abs(array_fft[i]/mu)))         # Cálculo del parámetro de comparación T, por ventana, para detectar ausencia de habla
+    
+    
+    spectral_estimator_att = np.zeros(np.shape(spectral_estimator_rnr),dtype=complex)
+
+    for i in range(0,cant_ventanas):                    # Asignación de atenuación por ventanas según su valor de T
+        
+        if T[i] >= -12:
+            spectral_estimator_att[i] = spectral_estimator_rnr[i]
+        
+        else:
+            spectral_estimator_att[i] = (10**(-30/20))*spectral_estimator_rnr[i]
+    
+    return spectral_estimator_att
+
+# Framing Step
+
+def framing (señal, duracion=2205, fs=44100, overlap=0):
+    
+    """
+    In the short-term analysis or frame-based processing, 
+    the audio signal is sliced into possibly overlapping N frames.
+    
+    señal: Array de señal de audio. (array)
+    
+    duracion: Duración del intervalo/frame en muestras.(int)
+    (entre 10 y 50 ms) (2205 muestras son 50 ms para fs=44100)
+    
+    fs: Frecuencia de muestreo. (int)
+    
+    """
+    
+    x = señal                           # Señal de audio a ser dividida en N cuadros (frames)
+    d = duracion                        # Longitud de cada uno de los N intervalos
+    
+    # Framing
+    
+    x_f = []                            # Array para los frames
+    
+    # x_f[0] = x[0:d]                     # Determinación del primer frame
+    
+    for i in range(0,int((len(x)-d+overlap)/d)):       # Asignación de los frames de la señal
+        
+        x_f.append(x[i*d-overlap:i*d-overlap+d])
+        
+        
+    # Windowing (Hamming Window)
+    
+    n = np.arange(0,d)                          # Vector n muestras de longitud del frame
+    
+    w = 0.54 - 0.46 * np.cos(2*np.pi*n/d)       # Ventana de Hamming
+    
+    for i in range(0,len(x_f)):                 # Aplicación de ventaneo a cada frame
+        
+        x_f[i] = w*x_f[i]
+        
+    return x_f
+
+
+# Short Time Energy
+
+def STE(señal, d=2205):
+    
+    x = framing(señal,duracion=d)      # Aplico el framing a la señal x,  se obtiene la señal x separada en frames y el ancho d de frames
+        
+    STE = np.zeros(len(x))         
+    
+    for i in range(0,len(STE)):     # Cálculo del STE para cada frame i de la señal
+        
+        STE[i] = np.sum(np.abs((x[i]))**2)/d
+    
+    return STE
+
+
+# Zero Crossing Rate ("Short Time Average Zero Crossing Rate")
+
+def ZCR(señal, d=2205):
+    
+    """
+    The ZCR show the level of noisiness of a given signal.
+    In fact, noisy recorded audio signals correspond to
+    high values of ZCR.
+    """
+    
+    x = framing(señal,duracion=d)
+      
+    
+    sgn = np.zeros((len(x),d))          # Array de ceros para definir la función "sign"
+    
+    for i in range(0,len(sgn)):      # Recorre cada frame de la señal dividida
+    
+        for n in range(0,d):         # Iteración en cada muestra de cada frame
+        
+            if x[i][n]>=0:           # Asignación de valores al array de sgn
+                sgn[i][n] = 1
+            
+            else:
+                sgn[i][n] = -1            
+        
+    ZCR = np.zeros(len(x))            
+    
+    for i in range(0,len(sgn)):
+        num = []
+        for n in range(0,d):
+            num.append(np.abs(sgn[i][n]-sgn[i][n-1]))  # Armado del vector de restas entre un elemento i de sgn y su anterior
+        
+        ZCR[i] = np.sum(num)/(2*d)                     # Sumatoria de los resultados de todas las restas, dividido el doble del largo de la ventana     
+        
+    return ZCR
+    
+
+def energia(x):
+    
+    e = np.sum(np.abs(x)**2)
+        
+    return e
+        
+
+def estimacion_max_ruido(x):
+    """
+    Cálculo del máximo de las primeras 1000 muestras de una señal.
+    La idea es tomar el valor máximo del ruido, para luego calcular el SNR de la señal.
+    También se asume que la señal comienza solo con ruido.
+    """
+    return np.max(abs(x[0:1000]))
--- main.py
+++ main.py
@@ -1,5 +1,140 @@
-import func
+from func import Boll_base, Boll_alt, synthesis, STE, ZCR, energia, estimacion_max_ruido
+import numpy as np
+import soundfile as sf
+import matplotlib.pyplot as plt
 
+#%%
+# Se importa la señal de referencia
 
+# import_s = sf.read('ClarinetHiNoise.wav')
+# import_s = sf.read('VegaHiNoise.wav')
+import_s = sf.read('GlockHiNoise.wav')
 
+señal = import_s[0]
+fs = import_s[1]
+señal_L = np.transpose(señal)[0]        # Se toma un solo canal de la señal estéreo
 
+# Aplicación de ambos métodos y sus síntesis.
+
+BB = Boll_base(señal_L, fs)
+
+señal_BB = synthesis(BB)
+
+BA = Boll_alt(señal_L, fs)
+
+señal_BA = synthesis(BA)
+#%%
+# Creación de archivos .wav con los resultados/señales filtradas mediante cada método.
+
+sf.write('Salida_BB_Glock.wav',np.real(señal_BB)/np.max(np.real(señal_BB)),fs)
+
+sf.write('Salida_BA_Glock.wav',np.real(señal_BA)/np.max(np.real(señal_BA)),fs)
+
+#%%
+# Análisis y comparación de los STE y ZCR de ambos métodos
+
+STE_señal = STE(señal_L)
+ZCR_señal = ZCR(señal_L)
+
+STE_BB = STE(señal_BB)
+ZCR_BB = ZCR(señal_BB)
+
+STE_BA = STE(señal_BA)
+ZCR_BA = ZCR(señal_BA)
+
+plt.figure(figsize=(15,6))
+plt.suptitle("Filtrado con método de Boll Básico")
+
+plt.subplot(1,2,1)
+plt.plot(STE_BB/np.max(STE_BB))
+plt.plot(STE_señal/np.max(STE_señal))
+plt.ylabel("Magnitud")
+plt.xlabel("Ventana")
+plt.title("Short Time Energy")
+
+plt.subplot(1,2,2)
+plt.plot(ZCR_BB/np.max(ZCR_BB))
+plt.plot(ZCR_señal/np.max(ZCR_señal))
+plt.ylabel("Magnitud")
+plt.xlabel("Ventana")
+plt.title("Zero-Crossing Rate")
+
+plt.tight_layout()
+plt.show()
+
+plt.figure(figsize=(15,6))
+plt.suptitle("Filtrado con método de Boll Alternativo (Sobresubstracción)")
+
+plt.subplot(1,2,1)
+plt.plot(STE_BA/np.max(STE_BA))
+plt.plot(STE_señal/np.max(STE_señal))
+plt.ylabel("Magnitud")
+plt.xlabel("Ventana")
+plt.title("Short Time Energy")
+
+plt.subplot(1,2,2)
+plt.plot(ZCR_BA/np.max(ZCR_BA))
+plt.plot(ZCR_señal/np.max(ZCR_señal))
+plt.ylabel("Magnitud")
+plt.xlabel("Ventana")
+plt.title("Zero-Crossing Rate")
+
+plt.tight_layout()
+plt.show()
+
+#%%
+# Análisis de "Relación de Energía" para la señal original, la señal_BB y la señal_BA
+
+# import_s_limpia = sf.read('ClarinetREF.wav')
+# import_s_limpia = sf.read('VegaREF.wav')
+# import_s_limpia = sf.read('GlockREF.wav')
+
+señal_limpia = import_s_limpia[0]
+señal_L_limpia = np.transpose(señal_limpia)[0]
+
+# El array de "resta" de cada caso corresponde al ruido resultante de la resta entre señal total y la señal limpia de cada caso.
+
+resta_original = señal_L - señal_L_limpia
+
+resta_BB = señal_BB - señal_L_limpia
+
+resta_BA = señal_BA - señal_L_limpia
+
+plt.figure(figsize=(15,6))
+
+# plt.plot(resta_original/np.max(resta_original),label="Original")
+# plt.plot(resta_BB/np.max(resta_BB),label="Boll Básico")
+# plt.plot(resta_BA/np.max(resta_BA),label="Boll Alternativo")
+plt.plot(resta_original,label="Original")
+plt.plot(resta_BB,label="Boll Básico")
+plt.plot(resta_BA,label="Boll Alternativo")
+plt.ylabel("Magnitud")
+plt.xlabel("Muestras")
+plt.title("Ruido Resultante")
+
+plt.legend()
+
+plt.tight_layout()
+plt.show()
+
+# Energía (se calculan y comparan los porcentajes de la energía de la señal total que representa cada ruido resultante)
+
+per_e_original = 100*energia(resta_original) / energia(señal_L)
+per_e_BB = 100*energia(resta_BB) / energia(señal_BB)
+per_e_BA = 100*energia(resta_BA) / energia(señal_BA)
+
+print("Porcentaje de energía del ruido resultante de la señal original:", per_e_original)
+print("Porcentaje de energía del ruido resultante de la señal original:", per_e_BB)
+print("Porcentaje de energía del ruido resultante de la señal original:", per_e_BA)
+
+#%%
+
+# SNR
+
+SNR_original = np.max(señal_L)/estimacion_max_ruido(señal_L)
+SNR_BB = np.max(señal_BB)/estimacion_max_ruido(señal_BB)
+SNR_BA = np.max(señal_BA)/estimacion_max_ruido(señal_BA)
+
+print("SNR para señal original:",np.round(SNR_original,2))
+print("SNR para señal Boll Básico:",np.round(np.real(SNR_BB),2))
+print("SNR para señal Boll Alternativo:",np.round(np.real(SNR_BA),2))
